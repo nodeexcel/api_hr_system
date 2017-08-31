a2501064599913440e597f6314498d19964cae8b
nodeexcel <vaibhav.excel2011@gmail.com>
2017-08-31 19:55:38 +0530

    Update file.js

--- src/models/file.js
+++ src/models/file.js
@@ -1,10 +1,11 @@
 export default function(sequelize, DataTypes) {
-    var details = sequelize.define('attendances', { // inserting data to database of attendance
+    var details = sequelize.define('attendance', { // inserting data to database of attendance
         id: { type: DataTypes.INTEGER, primaryKey: true },
         user_id: { type: DataTypes.INTEGER },
         timing: { type: DataTypes.STRING(1234) }
     }, {
-        timestamps: false
+        timestamps: false,
+        freezeTableName: true
     });
 
     return details
