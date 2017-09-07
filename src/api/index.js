83326619b0e4ac313425733d150208dd49ae55f3
Sumit <sumit@excellencetechnologies.in>
2017-09-07 11:29:22 +0530

    updated with post routes

--- src/api/index.js
+++ src/api/index.js
@@ -4,10 +4,10 @@ import controller from "../controller";
 export default () => {
     let api = Router();
 
-    api.route('/user/attendance/').get(controller.upload.attendance);
-    api.route('/attendance/update_time_by_employee').get(controller.upload.attendanceByEmployee);
-    api.route('/attendance/manual/').get(controller.fetch.manualUpdatedAttendance);
-    api.route('/attendance/approval').get(controller.fetch.approvalAction);
+    api.route('/user/attendance/').post(controller.upload.attendance);
+    api.route('/attendance/update_time_by_employee').post(controller.upload.attendanceByEmployee);
+    api.route('/attendance/manual/').post(controller.fetch.manualUpdatedAttendance);
+    api.route('/attendance/approval').post(controller.fetch.approvalAction);
     api.route('/reports/get_team_stats').post(controller.stats.teamStats);
 
     return api;
--- src/controller/index.js
+++ src/controller/index.js
@@ -1,6 +1,6 @@
 import upload from "./upload.js";
 import fetch from "./manualAttendance.js";
-import stats fro './employeeStats'
+import stats fro './employeeStats.js'
 
 export default {
     upload,
