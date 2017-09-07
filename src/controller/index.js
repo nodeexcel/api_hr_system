8da581b023c1c8890a2b7ac10b32c3061b51842c
nodeexcel <vaibhav.excel2011@gmail.com>
2017-09-07 11:37:56 +0530

    Update index.js

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,6 +1,6 @@
 import upload from "./upload.js";
 import fetch from "./manualAttendance.js";
-import stats fro './employeeStats.js'
+import stats from './employeeStats.js'
 
 export default {
     upload,
