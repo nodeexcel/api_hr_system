3b2b18962da9bbf73e6c427e76e16932bce03925
Sumit <sumit@excellencetechnologies.in>
2017-09-08 16:49:50 +0530

    updated

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,9 +1,8 @@
 import upload from "./upload.js";
 import fetch from "./manualAttendance.js";
-import stats fro './employeeStats.js'
 
 export default {
     upload,
-    fetch,
-    stats
+    fetch
+
 }
