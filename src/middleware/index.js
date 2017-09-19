ff4e550cd2e2bb60a9a4b62bdb66b8b5da18b28e
Sumit <sumit@excellencetechnologies.in>
2017-09-19 18:20:04 +0530

    updated

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,8 +3,6 @@ import db from '../db';
 
 module.exports = {
     verifyUserAction: function(req, res, next) {
-        console.log("vfsdgfkjsdhlkfjsdlk")
-        console.log(req.headers[2])
         let data = JSON.parse(req.body);
         helper.verify.verifyToken(data.token).then((user_id, action_list) => {
             helper.verify.verifyAction(data.action, action_list, function(err, responses) {
