aec56c636f40551f88a4affe1a049051e09fd151
Sumit <sumit@excellencetechnologies.in>
2017-09-26 16:07:00 +0530

    updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,19 @@
 import { Router } from 'express';
 import controller from "../controller";
-import auth from '../middleware'
 
 export default () => {
     let api = Router({ mergeParams: true });
 
-    api.route('/user/attendance/').post(auth.verifyUserAction, controller.upload.attendance);
-    api.route('/attendance/update_time_by_employee').post(auth.verifyUserAction, controller.upload.attendanceByEmployee);
-    api.route('/attendance/manual/').post(auth.verifyUserAction, controller.fetch.manualUpdatedAttendance);
-    api.route('/attendance/approval').post(auth.verifyUserAction, controller.fetch.approvalAction);
-    api.route('/reports/get_team_stats').post(auth.verifyUserAction, controller.stats.graphStats);
-    api.route('/reports/get_termination_joining_stats').post(auth.verifyUserAction, controller.stats.joiningTerminatiionStats);
-    api.route('/reports/get_employee_hours').post(auth.verifyUserAction, controller.stats.EmployeeHours);
-    api.route('/reports/get_monthly_report').post(auth.verifyUserAction, controller.stats.monthlyReport);
+    api.route('/user/attendance/').post(controller.upload.attendance);
+    api.route('/attendance/update_time_by_employee').post(controller.upload.attendanceByEmployee);
+    api.route('/attendance/manual/').post(controller.fetch.manualUpdatedAttendance);
+    api.route('/attendance/approval').post(controller.fetch.approvalAction);
+    api.route('/reports/get_team_stats').post(controller.stats.graphStats);
+    api.route('/reports/get_termination_joining_stats').post(controller.stats.joiningTerminatiionStats);
+    api.route('/reports/get_employee_hours').post(controller.stats.EmployeeHours);
+    api.route('/reports/get_monthly_report').post(controller.stats.monthlyReport);
     api.route('/xyz').get(controller.stats.abc);
+    api.route('/reports/get_monthly_performance').post(controller.stats.monthlyPerformance);
 
     return api;
 }
