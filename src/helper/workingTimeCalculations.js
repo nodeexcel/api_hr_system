a589aa640f8325b06e2ffedc7c4c9db593ca4186
Sumit <sumit@excellencetechnologies.in>
2017-10-03 16:33:45 +0530

    updated

--- src/helper/workingTimeCalculations.js
+++ src/helper/workingTimeCalculations.js
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import db from '../db';
+import config from '../../../config.json'
 import _ from 'lodash';
 
 module.exports = {
@@ -10,44 +11,48 @@ module.exports = {
             }
             let output;
             let data = [];
-            db.attendance.get_monthly_attendance(body.month, body.year, userid, function(monthly_data) { //time array for a id
-                let month = new Date(Date.parse(body.month + " 1, 2012")).getMonth() + 1;
-                let no_of_days = (moment(body.year + "-" + month, "YYYY-MM").daysInMonth()) + 1;
-                let days_of_month = _.range(1, no_of_days);
-                let daily = [];
-                _.forEach(days_of_month, function(day) {
-                    let entryTime = 0;
-                    let exitTime = 0;
-                    let start_entry = 0;
-                    let active_hours = 0;
-                    _.forEach(monthly_data, function(times) {
-                        let date = moment((times).slice(0, -2)).format('DD');
-                        if (day == date) {
-                            if (entryTime) {
+            if (userid != 'null' && userid != 0) {
+                db.attendance.get_monthly_attendance(body.month, body.year, userid, function(monthly_data) { //time array for a id
+                    let month = new Date(Date.parse(body.month + " 1, 2012")).getMonth() + 1;
+                    let no_of_days = (moment(body.year + "-" + month, "YYYY-MM").daysInMonth()) + 1;
+                    let days_of_month = _.range(1, no_of_days);
+                    let daily = [];
+                    _.forEach(days_of_month, function(day) {
+                        let entryTime = 0;
+                        let exitTime = 0;
+                        let start_entry = 0;
+                        let active_hours = 0;
+                        _.forEach(monthly_data, function(times) {
+                            let date = moment((times).slice(0, -2)).format('DD');
+                            if (day == date) {
+                                if (entryTime) {
 
-                                exitTime = new Date(times).getTime();
-                                active_hours = active_hours + (exitTime - entryTime);
-                                entryTime = 0;
-                            } else {
-                                if (start_entry) {
-                                    entryTime = new Date(times).getTime();
+                                    exitTime = new Date(times).getTime();
+                                    active_hours = active_hours + (exitTime - entryTime);
+                                    entryTime = 0;
                                 } else {
-                                    entryTime = new Date(times).getTime();
-                                    start_entry = new Date(times).getTime();
+                                    if (start_entry) {
+                                        entryTime = new Date(times).getTime();
+                                    } else {
+                                        entryTime = new Date(times).getTime();
+                                        start_entry = new Date(times).getTime();
+                                    }
                                 }
                             }
-                        }
-                    })
-                    daily.push({
-                        day: (day + " " + moment(month, 'MM').format('MMMM')),
-                        total_hours: { hours: _.floor(((exitTime - start_entry) / 3600000) % 24), minutes: _.floor(((exitTime - start_entry) / 60000) % 60), total_time: _.floor(((exitTime - start_entry) / 3600000) % 24) + "." + _.floor((_.floor(((exitTime - start_entry) / 60000) % 60) * 5) / 3) },
-                        active_hours: { hours: _.floor((active_hours / 3600000) % 24), minutes: _.floor(((active_hours) / 60000) % 60), total_time: _.floor((active_hours / 3600000) % 24) + "." + _.floor((_.floor(((active_hours) / 60000) % 60) * 5) / 3) }
-                    })
-                });
-                data.push({ user_id: userid, day_wise_detail: daily });
-                output = { error: 0, message: "", data: data };
-                resolve(output)
-            })
+                        })
+                        daily.push({
+                            day: (day + " " + moment(month, 'MM').format('MMMM')),
+                            total_hours: { hours: _.floor(((exitTime - start_entry) / 3600000) % 24), minutes: _.floor(((exitTime - start_entry) / 60000) % 60), total_time: _.floor(((exitTime - start_entry) / 3600000) % 24) + "." + _.floor((_.floor(((exitTime - start_entry) / 60000) % 60) * 5) / 3) },
+                            active_hours: { hours: _.floor((active_hours / 3600000) % 24), minutes: _.floor(((active_hours) / 60000) % 60), total_time: _.floor((active_hours / 3600000) % 24) + "." + _.floor((_.floor(((active_hours) / 60000) % 60) * 5) / 3) }
+                        })
+                    });
+                    data.push({ user_id: userid, day_wise_detail: daily });
+                    output = { error: 0, message: "", data: data };
+                    resolve(output)
+                })
+            } else {
+                reject({ error: 1, message: config.errMsg1, data: "" })
+            }
 
         })
     },
